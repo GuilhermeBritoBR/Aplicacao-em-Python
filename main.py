9defc7f56f127e7f7394c67ae19cadc01756b5cd
Guilherme Santiago de Brito <125681424+GuilhermeBritoBR@users.noreply.github.com>
2023-03-19 14:21:05 -0300

    19/03/2023
    Error: cálculos de segundos
    ainda sem a interface gráfica

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Distância")
+Distância=int(input("Quantos quilometos?"))
+Minutos = int( input("Quantos minutos?: ") )
+Segundos = int( input("Quantos segundos?: "))
+Segundosemdecimal=(Segundos/)
+Tempo=(Minutos+Segundosemdecimal)
+Pace=(Tempo/Distância)
+print("Pace")
+print(Pace)
+
+
